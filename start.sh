34c52e836a24cee9eccfab423e3113deedc86c96
Kyriakos Kritikos <domcretan@gmail.com>
2023-03-11 20:53:01 +0200

    Some additional docker changes

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 sudo docker network create net
 sudo docker run -d -v /home/ubuntu/mysql:/var/lib/mysql -v "$(pwd)"/mysql:/docker-entrypoint-initdb.d -e MYSQL_RANDOM_ROOT_PASSWORD=yes --network net --name mysql mysql
-sudo docker run -d -p 30000:30000 -v "$(pwd)"/tomcat/tomcat-users.xml:/usr/local/tomcat/conf/tomcat-users.xml --network net --name service service:latest
+sudo docker run -d -p 8080:30000 -v "$(pwd)"/tomcat/tomcat-users.xml:/usr/local/tomcat/conf/tomcat-users.xml --network net --name service service:latest
